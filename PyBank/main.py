90198c69207e6b4cb4e0209bd842c0c3fa1a394f
Rhiana Schafer <rhiana.schafer@northwestern.edu>
2022-03-19 17:14:27 -0500

    Forgot to create the export txt file. NOW PyBank is complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,3 @@
-#Your analysis should look similar to the following:
-#  Financial Analysis
-#  Total Months: 86
-#  Total: $22564198
-#  Average Change: $-8311.11
-#  Greatest Increase in Profits: Aug-16 ($1862002)
-#  Greatest Decrease in Profits: Feb-14 ($-1825558)
-
-#Your final script should both print the analysis to the terminal and
-#export a text file with the results.
-
 import os
 import csv
 import numpy as np
@@ -20,34 +9,43 @@ bank_csv = os.path.join("Resources", "budget_data.csv")
 with open(bank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
     for row in csvreader:
         dates.append(row[0])
         profit_losses.append(int(row[1]))
 
-print("Financial Analysis")
+header = "Financial Analysis"
+print(header)
 
 #Finds the total number of months included in the dataset
-print(f"Total Months: {len(dates)}")
+line1 = f"Total Months: {len(dates)}"
+print(line1)
 
 #Finds the net total amount of "Profit/Losses" over the entire period
-print(f"Total: ${sum(profit_losses)}")  
+line2 = f"Total: ${sum(profit_losses)}"
+print(line2)
 
 #Finds the changes in "Profit/Losses" over the entire period, 
 #and then the average of those changes
 changes = []
 for x in range(len(profit_losses)-1):
     changes.append(profit_losses[x+1] - profit_losses[x])
-
 change_average = round(np.average(changes), 2)
-print(f"Average change: ${change_average}")
+line3 = f"Average change: ${change_average}"
+print(line3)
 
 #Finds the greatest increase in profits (date and amount) over the entire period
 big_profit = max(changes)
 big_profit_day = dates[changes.index(big_profit)+1]
-print(f"Greatest Increase in Profits: {big_profit_day} (${big_profit})")
-
+line4 = f"Greatest Increase in Profits: {big_profit_day} (${big_profit})"
+print(line4)
 #Finds the greatest decrease in profits (date and amount) over the entire period
 big_loss = min(changes)
 big_loss_day = dates[changes.index(big_loss)+1]
-print(f"Greatest Decrease in Profits: {big_loss_day} (${big_loss})")
+line5 = f"Greatest Decrease in Profits: {big_loss_day} (${big_loss})"
+print(line5)
+
+#Creates output file
+output = [header,"\n", line1, "\n", line2, "\n", line3, "\n", line4, "\n", line5]
+analysis_path = os.path.join("Analysis", "analysis.txt")
+with open(analysis_path, "w") as datafile:
+    datafile.writelines(output)
