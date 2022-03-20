557b1a227f80ebb5a84868e52c34fdc01b962476
Rhiana Schafer <rhiana.schafer@northwestern.edu>
2022-03-19 22:30:41 -0500

    Writing ReadMe, Setting up code framework for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,4 @@
-## PyPoll Instructions
-
-#In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
-
-#You will be given a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). 
-# The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
-# Your task is to create a Python script that analyzes the votes and calculates each of the following:
-    #* The total number of votes cast
-    #* A complete list of candidates who received votes
-    #* The percentage of votes each candidate won
-    #* The total number of votes each candidate won
-    #* The winner of the election based on popular vote.
-
 #Your analysis should look similar to the following:
-
  # Election Results
   #Total Votes: 369711
   #Charles Casper Stockham: 23.049% (85213)
@@ -21,3 +7,42 @@
   #Winner: Diana DeGette
 
 #Your final script should both print the analysis to the terminal and export a text file with the results.
+
+import os
+import csv
+
+#Accessing csv data and making workable
+voter_ids = []
+counties = []
+candidates = []
+poll_csv = os.path.join("Resources", "election_data.csv")
+with open(poll_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    for row in csvreader:
+        voter_ids.append(row[0])
+        counties.append(row[1])
+        candidates.append(row[2])
+
+header = "Election Results"
+print(header)
+
+#The total number of votes cast
+line1 = f"Total Votes: {len(voter_ids)}"
+print(line1)
+
+#A complete list of candidates who received votes, 
+# along with the percentage of votes each candidate won, 
+# and the total number of votes each candidate won
+line234 = f"{name}: {percent}% ({total_votes})"
+print(line234)
+
+#The winner of the election based on popular vote.
+line5 = f"Winner: {winner)}"
+print(line1)
+
+#Creates output file
+output = [header,"\n", line1, "\n", line2, "\n", line3, "\n", line4, "\n", line5]
+analysis_path = os.path.join("Analysis", "analysis.txt")
+with open(analysis_path, "w") as datafile:
+    datafile.writelines(output)
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # python-challenge
+Python Homework for Data Science Bootcamp
+Rhiana Schafer, 3/19/2022
 
-#testing git push
+This code contains two solutions
+
+## PyBank 
+
+A Python script that analyzes the records to calculate each of the following:
+
+* The total number of months included in the dataset
+
+* The net total amount of "Profit/Losses" over the entire period
+
+* The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+* The greatest increase in profits (date and amount) over the entire period
+
+* The greatest decrease in profits (date and amount) over the entire period
+
+
+
+## PyPoll
+
+A Python script that analyzes the votes and calculates each of the following:
+
+* The total number of votes cast
+
+* A complete list of candidates who received votes
+
+* The percentage of votes each candidate won
+
+* The total number of votes each candidate won
+
+* The winner of the election based on popular vote.
